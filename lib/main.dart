57d0145040d3306e3cd74b00a45f9d239a57d868
tbradford8 <tbradford8@gatech.edu>
2021-08-21 00:35:02 -0700

    Added Column

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,29 @@ class DeepApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.blueGrey[900],
-          appBar: AppBar(
-            title: Text("Deep waves"),
-            backgroundColor: Colors.deepOrange[900],
-          ),
-          body: SafeArea(
-            child: Center(
-              child: Image(
-                image: AssetImage('images/83606.jpg'),
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          title: Text("Deep waves"),
+          backgroundColor: Colors.deepOrange[900],
+        ),
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.all(50.0),
+            child: Column(children: [
+              Text("Look into the Deep"),
+              Container(
+                height: 300,
+                // padding: EdgeInsets.fromLTRB(50, 51, 52, 53),
+                child: Center(
+                  child: Image(
+                    image: AssetImage('images/83606.jpg'),
+                  ),
+                ),
               ),
-            ),
-          )),
+            ],),
+          )
+        ),
+      ),
     );
   }
 }
