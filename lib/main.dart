5383a410cef39b6a3dbdd22029368a8f0161a9f7
tbradford8 <tbradford8@gatech.edu>
2021-08-21 10:59:06 -0700

    Played with some styling a bit

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,36 @@ class DeepApp extends StatelessWidget {
           backgroundColor: Colors.deepOrange[900],
         ),
         body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.all(50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-              Text("Look into the Deep"),
+            child: Container(
+          margin: EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
               Container(
-                height: 300,
+                child: Text(
+                  "Look into the Deep",
+                  style: TextStyle(
+                      color: Colors.pinkAccent, fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                color: Colors.black54,
+              ),
+              Container(
+                height: 500,
                 // padding: EdgeInsets.fromLTRB(50, 51, 52, 53),
                 child: Center(
-                  child: Image(
-                    image: AssetImage('images/83606.jpg'),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/83606.jpg'),
+                    backgroundColor: Colors.blueAccent,
+                    minRadius: 50,
                   ),
                 ),
               ),
-            ],),
-          )
-        ),
+              Row(
+                children: [Icon(Icons.car_repair), Icon(Icons.ac_unit)],
+              )
+            ],
+          ),
+        )),
       ),
     );
   }
